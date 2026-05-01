716e2e53aa07d19818ad1725b415e13e447b95cc
Riley14-coding <rileygaetz@gmail.com>
2026-05-01 10:00:02 -0500

    working on MVC

--- Scripts/MVC/PlayerModel.gd
+++ Scripts/MVC/PlayerModel.gd
@@ -1,1 +1,11 @@
 extends Node
+
+class_name PlayerModel
+
+@export var health = 5;
+
+signal health_changed(new_health)
+
+func set_health(value: int) -> void:
+	health = clamp(value, 0, 5)
+	emit_signal("health_changed", health)
